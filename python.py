597748555d9b34f67bf73a27f168119cc28aff1f
Piyush Darji <piyushdarji62@gmail.com>
2020-08-27 15:19:25 +0530

    findfucntion

--- python.py
+++ python.py
@@ -26,3 +26,18 @@
 # x = "hello"
 # a = x.find("l")
 # print(a)
+
+
+
+# x = 2
+# for i in range(x):
+#     x += 1
+#     print(x)
+
+
+
+# tup = (0,1,2)
+# lst = [tup]
+# lst.append(3)
+# lst.pop(0)
+# print(len(lst))
