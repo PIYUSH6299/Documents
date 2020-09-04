b9ff84b576167989148f489d05cb366a81eae5ea
Piyush Darji <piyushdarji62@gmail.com>
2020-09-05 00:46:41 +0530

    erro.py

---
+++ Counter.py
@@ -0,0 +1,25 @@
+# from collections import Counter
+
+# # x = [1,2,3,4,5,6,8,7,8,8,9,6,5,4,5,2,3,9,]
+# x = "piyush mayank artiben piyush dipaben"
+# words = x.split()
+# y = Counter(words) 
+# y += Counter()
+# print(y)
+
+
+
+# import datetime
+
+# today = datetime.date.today()
+# print(today.hour)
+
+
+import pdb
+
+x = [1,1,2,3]
+y = 4
+z = 6
+result = x + y
+pdb.set_trance()
+print(result)
---
+++ Error_exception.py
@@ -0,0 +1,92 @@
+# try:
+#     result = 10 + 10
+# except:
+#     print("Hey looks like you aren't adding currectly")
+# else:
+#     print("It went well!")
+#     print(result)
+
+
+
+
+# try:
+#     f = open('testfile', 'w')
+#     f.write('write a test line')
+# except:
+#     print('All the type of error')
+# finally:
+#     print('I always run')
+
+
+
+# def number():
+#     while True:
+        
+#         try:
+#             result = int(input("Provide a number: "))
+#         except:
+#             print("oops!!! that is not number \n")
+#             continue
+#         else:
+#             print("Thank you")
+#             break
+#         finally:
+#             print("End of the session")
+#             print("I always run at the end \n")
+# (number())
+
+
+
+
+# def ask():
+#     piyush = True
+#     while piyush: # True
+
+#         try:
+#             n = int(input("Input an integer: "))
+#         except:
+#             print("An error occured! please try again \n")
+#             continue
+#         else:
+#             piyush = False # break
+#     print("Your number squared is: ")
+#     print(n**2)
+# print(ask())
+        
+
+# # Decorators
+# def func1():
+#         print('This is a first line')
+#         print('This is a second line')
+# func1()
+
+# def newfunc():
+#     print('This is new')
+# newfunc()
+     
+# @func1
+# def newfunc():
+#     print('This is new')
+# newfunc()
+
+
+
+# x = "piyush"
+# for i in x:
+#     print(i)
+
+# x = iter(x)
+# # next(x)
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+
+
+# x = [1,2,3,4,5]
+# for i in x:
+#     yield(i**3)
+#     print(i   )
+    
